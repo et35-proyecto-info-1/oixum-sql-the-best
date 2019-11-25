73d50bc3555758fcc9911f6ec6fb0e36a7c845f0
jonathan113 <50118036+jonathan113@users.noreply.github.com>
2019-11-25 11:57:07 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ var express = require('express');
 var connection = require('./connection');
 var exphbs = require('express-handlebars');
 
-
 var path = require('path');
 
 var app = express();
 
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs());
@@ -16,19 +17,25 @@ app.get('/', function(req, res) {
   res.render('home');
 });
 
+function boton(){
+var tConsulta=text(document.getElementById("consulta").value)
+}
+
 app.post('/', function(req, res) {
   const query = req.body.query;
   connection.query(query, function (error, results, fields) {
-
+    console.log(results);
+    console.log(fields);
     res.render('home', {
-      resultados: JSON.stringify(results),
-      columnas: JSON.stringify(fields),
+      resultados: results,
+      columnas: fields,
       error: error,
       text: query
-
     });
   });
 });
+
+
 
 app.listen(3000, function() {
   connection.connect();
